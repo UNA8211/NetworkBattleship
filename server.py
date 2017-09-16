f2358f754ffe91b848077be08efe55d4ddf73ddf
Ethan Peterson <cap2010.ep@gmail.com>
2017-09-16 11:32:59 -0600

    Add skeletons of functions to populate later.

--- server.py
+++ server.py
@@ -1,3 +1,18 @@
 # setup 10 x 10 matrices for player board and opponent board (Python is stupid)
 ownBoard = [[""] * 10 for i in range(10)]
 oppBoard = [[""] * 10 for i in range(10)]
+
+# checkFire takes in a set of coordinates (target of an opponent's shot)
+#   and assesses the result of the shot. This result is written to the Message.
+def checkFire(x, y):
+    return "Hopefully it didn't do damage"
+
+# readBoard takes a file representing a board
+#   and reads it into the given 10 x 10 matrix
+def readBoard(board, file):
+    return "bitch lasagna"
+
+# readBoard takes a 10 x 10 matrix representing a board
+#   and writes it to the given file
+def writeBoard(board, file):
+    return "show bob and vagene"
