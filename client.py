e370393ac7d52beac37f0213aa96401ae9d9bb0d
Ethan Peterson <cap2010.ep@gmail.com>
2017-09-17 17:46:58 -0600

    Ensure client is sending the correct coordinates.

--- client.py
+++ client.py
@@ -6,19 +6,18 @@ def main():
     if len(args) != 4:
         print ("ERROR: Invalid number of arguments")
         return 1
-    
+
     ip = args[0]
-    
+
     port = re.match(PORT_PATTERN, args[1], flags=0)
     if port is None:
         print ("ERROR: Invalid port")
         return 1
 
-    coords = args[2] + " " + args[3]
-    fire(ip, port.group(), coords)
+    fire(ip, port.group(), args[2], args[3])
 
-def fire(ip, port, coords):
-    parameters = {"x" : coords[0:1], "y" : coords[-1:]}
+def fire(ip, port, x, y):
+    parameters = {"x" : x, "y" : y}
 
     r = requests.post("http://" + ip + ":" + port, data=parameters, timeout=30)
     r.status_code
@@ -27,11 +26,11 @@ def fire(ip, port, coords):
         #TODO: add the response as data to send to own server
         r = requests.post("http://localhost:5000", timeout=30)
         r.status_code
-    
+
 IP_PATTERN = "\d{3}\.\d{3}\.\d(\d{1,2})?\.\d(\d{1,2})?"
 PORT_PATTERN = "^\d{1,4}"
 COORDINATE_PATTERN = "\d \d"
 
-            
+
 if __name__ == "__main__":
     main()
